43cd2a84c0844ed54b409d2c668ca3b604a971ae
sonakshi1003 <sujata3dutta@gmail.com>
2020-04-30 16:29:56 +0530

    reset

--- js/Game.js
+++ js/Game.js
@@ -1,14 +1,11 @@
 class Game {
-  constructor(){
-
-  }
+  constructor(){}
 
   getState(){
     var gameStateRef  = database.ref('gameState');
     gameStateRef.on("value",function(data){
        gameState = data.val();
     })
-
   }
 
   update(state){
@@ -31,12 +28,16 @@ class Game {
 
     car1 = createSprite(100,200);
     car1.addImage("car1",car1_img);
+
     car2 = createSprite(300,200);
     car2.addImage("car2",car2_img);
+
     car3 = createSprite(500,200);
     car3.addImage("car3",car3_img);
+
     car4 = createSprite(700,200);
     car4.addImage("car4",car4_img);
+
     cars = [car1, car2, car3, car4];
   }
 
@@ -64,12 +65,16 @@ class Game {
 
         //position the cars a little away from each other in x direction
         x = x + 200;
+        
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
         cars[index-1].x = x;
         cars[index-1].y = y;
 
         if (index === player.index){
+          stroke(10);
+          fill("red");
+          ellipse(x, y, 60, 60);
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y;
